45b3a715dafddb9191e8c56241f699fd2dbe76f4
heinparn <heinparn@greeny.cs.tlu.ee>
2016-09-14 08:55:00 +0300

    HelloWorld

---
+++ helloWorld.php
@@ -0,0 +1,10 @@
+<?php 
+	
+	$firstName = "Heino";
+	$lastName = "Pärn";
+	echo $firstName; //trükib välja muutuja väärtuse
+	echo $lastName;
+	echo "Hello World!";
+	//komeaare
+	echo $firstName." ".$lastName;
+?>
